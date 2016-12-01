c89a8aa31452ed8a6ae8a6e921205e137ac88ed9
matthewsamuel95 <msamuel3@mail.csuchico.edu>
2016-12-01 11:34:25 -0800

    welcome message

--- DealorNoDeal.js
+++ DealorNoDeal.js
@@ -7,6 +7,9 @@
   var turns=6;
   var gamesPlayed=0;
 function start(){
+  document.getElementById("remain").innerHTML =
+  "Hello Welcome, to Deal or Deal !<br> Start By Picking Any 6 Cases!";
+
   var nums=[1,5,10,25,50,75,100,200,300,400,500,750,1000,2500,5000,10000,25000,
     50000,100000,200000,300000,400000,500000,750000,1000000];
 
@@ -70,7 +73,7 @@ function myFunction(buttonP){
     average=average.toFixed(2);
     document.getElementById("message").innerHTML
     //money format with commas found online
-    = "Offer: $"+average.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    = "DEAL OR NO DEAL? Offer: $"+average.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
     //window.alert("$"+average.toFixed(2));
 
     //if they select deal take them to new page
@@ -127,7 +130,7 @@ function calculateResult(){
   var theAverage;
   theAverage= getCookie("average");
   document.getElementById("sub").innerHTML =
-  "Offer: $"+theAverage.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+  "You won: $"+theAverage.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
 }
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
